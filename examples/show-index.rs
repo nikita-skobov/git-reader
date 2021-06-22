8f6b9e0451c256344becad4d7a21c2a376e731a7
nikita-skobov <nskobov.websites@gmail.com>
2021-06-21 22:51:17 -0500

    changes show-index to use the very fast IDXFileLight

--- examples/show-index.rs
+++ examples/show-index.rs
@@ -1,6 +1,6 @@
 use std::{path::PathBuf, io};
 use git_walker::{ioerr, object_database, ioerre, object_id::{hex_u128_to_str, PartialOid, hash_str_to_oid}};
-use object_database::{loose::{tree_object_parsing::TreeObject, commit_object_parsing::{ParseCommit, CommitOnlyTreeAndParents}}, packed::{open_pack_file_ex, DATA_STARTS_AT, open_idx_file}};
+use object_database::{loose::{tree_object_parsing::TreeObject, commit_object_parsing::{ParseCommit, CommitOnlyTreeAndParents}}, packed::{open_pack_file_ex, DATA_STARTS_AT, open_idx_file, open_idx_file_light}};
 use object_database::loose::UnparsedObjectType;
 
 /// This program is similar to git-show-index
@@ -11,12 +11,11 @@ pub fn realmain() -> io::Result<()> {
     let idxpath = args.get(1)
         .ok_or_else(|| ioerr!("Must provide a path to the index file"))?;
 
-    let idx = open_idx_file(idxpath)?;
-
-    idx.walk_all_oids(|oid| {
+    let idx = open_idx_file_light(&idxpath)?;
+    idx.walk_all_oids_with_index_and_from(None, |oid, fanout_index| {
         let oid_str = hex_u128_to_str(oid);
-        match idx.get_packfile_index_of_oid(oid) {
-            Ok(Some(i)) => {
+        match idx.find_packfile_index_from_fanout_index(fanout_index) {
+            Some(i) => {
                 println!("{} {}", i, oid_str);
             }
             _ => {
